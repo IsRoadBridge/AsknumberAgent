3df29a3f9ed136aa16c04b151f152319180d389f
IsRoadBridge <2849402866@qq.com>
2026-06-15 11:38:08 +0800

    Revert "feat：修复问题并启动项目"
    This reverts commit 1bcd585b

--- app/core/context.py
+++
@@ -1,4 +0,0 @@
-from contextvars import ContextVar
-
-request_id_ctx_var = ContextVar("request_id", default="1")
-
--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-import uuid
+# This is a sample Python script.
 
-from fastapi import FastAPI, Request
-
-from app.api.routers.query_router import query_router
-from app.core.context import request_id_ctx_var
-from app.core.lifespan import lifespan
-
-# 创建FastAPI应用，并注册生命周期函数
-app = FastAPI(lifespan=lifespan)
-
-# 注册路由
-app.include_router(query_router)
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-# 添加中间件，在每个请求中生成唯一的request_id
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    # 调用路径函数之前
-    request_id_ctx_var.set(uuid.uuid4())
-    # 调用路径函数
-    response = await call_next(request)
-    # 调用路径函数之后
-    return response
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    import uvicorn
+    print_hi('PyCharm')
 
-    uvicorn.run(app, host="0.0.0.0", port=8000,reload=True)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
